edfd81d4527f7a0b3c25c1c127333bd64ae51aad
susahin80 <susahin80@outlook.com.tr>
2023-03-15 20:39:13 +0300

    create and use custom useGames hook

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios, { CanceledError } from 'axios';
 
 export default axios.create({
     baseURL: 'https://api.rawg.io/api',
@@ -6,3 +6,5 @@ export default axios.create({
         key: '41f7cc3e746f40f09082a9d554618517',
     },
 });
+
+export { CanceledError };
