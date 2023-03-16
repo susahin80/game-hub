623b8be10d60ee2c26b6bff8975704d4241b24f3
susahin80 <susahin80@outlook.com.tr>
2023-03-16 13:32:06 +0300

    sort the games

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ import { Genre } from './hooks/useGenres';
 export interface GameQuery {
     genre: Genre | null;
     platform: Platform | null;
+    sortOrder: string;
 }
 
 function App() {
@@ -49,7 +50,12 @@ function App() {
                         }
                         selectedPlatform={gameQuery.platform}
                     />
-                    <SortSelector />
+                    <SortSelector
+                        onSelectSortOrder={(sortOrder) =>
+                            setGameQuery({ ...gameQuery, sortOrder })
+                        }
+                        sortOrder={gameQuery.sortOrder}
+                    />
                 </HStack>
                 <GameGrid gameQuery={gameQuery} />
             </GridItem>
--- src/components/image-url.ts
+++ src/components/image-url.ts
@@ -2,6 +2,8 @@
 // to: https://media.rawg.io/media/crop/600/400/games/618/618c2031a07bbff6b4f611f10b6bcdbc.jpg
 
 const getCroppedImageUrl = (url: string) => {
+    if (!url) return '';
+
     const target = 'media/';
     const index = url.indexOf(target) + target.length;
     return url.slice(0, index) + 'crop/600/400/' + url.slice(index);
