32324d883988d719ce48efefcc6610f96617c330
susahin80 <susahin80@outlook.com.tr>
2023-03-16 11:36:31 +0300

    highlight the selected genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -11,8 +11,9 @@ import getCroppedImageUrl from './image-url';
 
 interface Props {
     onSelectGenre: (genre: Genre) => void;
+    selectedGenre: Genre | null;
 }
-const GenreList = ({ onSelectGenre }: Props) => {
+const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
     const { data, isLoading, error } = useGenres();
 
     if (error) return null;
@@ -30,6 +31,11 @@ const GenreList = ({ onSelectGenre }: Props) => {
                             src={getCroppedImageUrl(genre.image_background)}
                         />
                         <Button
+                            fontWeight={
+                                genre.id === selectedGenre?.id
+                                    ? 'bold'
+                                    : 'normal'
+                            }
                             onClick={() => onSelectGenre(genre)}
                             fontSize="lg"
                             variant="link"
